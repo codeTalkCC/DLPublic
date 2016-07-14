b2d1f80fd8405e04de6bbb76e00c6f993406e86c
Jone <Jone@JoneMacBookPro.lan>
2016-07-14 20:25:25 +0800

    Initial commit

---
+++ Example/DLPublicDemo/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  DLPublicDemo
+//
+//  Created by xfjiang on 07/14/2016.
+//  Copyright (c) 2016 xfjiang. All rights reserved.
+//
+
+@import UIKit;
+#import "DLAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([DLAppDelegate class]));
+    }
+}
